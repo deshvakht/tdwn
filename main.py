1a97ffe84c082bf1b6ca46b4de501a0cd5f6ccba
mayank-ind <83276551+mayank-ind@users.noreply.github.com>
2023-01-22 14:01:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,14 +45,14 @@ bot = Client("bot",
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
 auth_users = [
-    int(chat) for chat in os.environ.get("AUTH_USERS","1937616968,5130707822,5679777302").split(",") if chat != ''
+    int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != ''
 ]
 sudo_users = auth_users
 sudo_groups = [
-    int(chat) for chat in os.environ.get("GROUPS","-654849862").split(",") if chat != ''
+    int(chat) for chat in os.environ.get("GROUPS",).split(",") if chat != ''
 ]
 sudo_groups = [
-    int(chat) for chat in os.environ.get("GROUPS","-654849862").split(",") if chat != ''
+    int(chat) for chat in os.environ.get("GROUPS").split(",") if chat != ''
 ]
 
 keyboard = InlineKeyboardMarkup(
